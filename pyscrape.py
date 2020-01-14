dfc681683d97c139c6c459162e2a9b8ab7b4c9ba
scoopgracie <scoopgracie@gmail.com>
2020-01-14 08:14:30 -0800

    Add docstrings
    All functions and classes have docstrings. Closes #8.

--- pyscrape.py
+++ pyscrape.py
@@ -2,6 +2,7 @@
 import requests
 from bs4 import BeautifulSoup
 class Page:
+    '''Page - Page.page = beautiful soup object; Page.request = requests request object'''
     def __init__(self, soup, request):
         self.page = soup
         self.request = request
@@ -9,6 +10,7 @@ class Page:
 caching = True
 __cache = {}
 def get(url, use_cache = True):
+    '''get(url, use_cache=True) - get url, skip cache if use_cache == False, will probably raise a Beautiful Soup or Requests exception on error'''
     if caching and use_cache and url in __cache.keys():
         return __cache[url]
     else:
@@ -21,10 +23,12 @@ def get(url, use_cache = True):
 
 
 def uncache(url):
+    '''uncache(url) - remove url from the cache, raise ValueError if page not in cache'''
     global __cache
     del __cache[url]
 
 
 def empty_cache():
+    '''empty_cache() - empty the cache'''
     global __cache
     __cache = {}
